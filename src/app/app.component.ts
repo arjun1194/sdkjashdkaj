08a46644e970ae7a89ef968874e6421feb328b56
Arjun Bhardwaj <arjunbhd1194@gmail.com>
2021-10-21 23:51:39 +0530

    Add Contact Functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Contact from './models/Contact';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,81 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demo-app';
+  count = 0;
+  initialChar = '';
+  showDiv = false;
+
+  searchBox = '';
+  /**
+   *  A dictionary with key value pairs like,
+   *  'a': [ {firstName: 'Arjun', lastName: 'Bhardwaj', company: 'Google'} ],
+   *  's': [{firstName: 'Salman', lastName: 'Khan', company: 'Google'}]
+   */
+  contactsMap: Map<string, Array<Contact> > = new Map<string, Array<Contact>>();
+
+  firstName = '';
+  lastName = '';
+  company = '';
+  contactCount = 0;
+
+  allContact: Contact[] = new Array<Contact>();
+
+  temporaryContact(): Contact {
+    return new Contact(
+      this.firstName,
+      this.lastName,
+      this.company
+    );
+  }
+
+  addContactButton(): void  {
+    // tslint:disable-next-line:no-non-null-assertion
+    const contact = this.temporaryContact();
+    this.addContact(contact);
+  }
+
+  public getAllContact(): void {
+    this.contactsMap.forEach((element) => {
+      this.allContact.push(...element);
+    });
+  }
+
+  private addContact(contact: Contact): void {
+    const firstLetter = contact.firstName.charAt(0).toLowerCase();
+    if (this.contactsMap.has(firstLetter)){
+      this.contactsMap.get(firstLetter)?.push(contact);
+    } else {
+      const newArray = new Array<Contact>();
+      newArray.push(contact);
+      this.contactsMap.set(firstLetter, newArray);
+    }
+    this.getAllContact();
+    this.contactCount = this.contactCount + 1;
+  }
+
+  /**
+  addContact(): void {
+    console.log('inside add contact');
+    this.initialChar = this.first.charAt(0);
+    if (this.first !== '' || this.last !== '' || this.company !== ''){
+      this.count = this.count + 1;
+      this.eachContact = {
+        firstName: this.first,
+        lastName: this.last,
+        companyName: this.company,
+        initialChar: this.first.charAt(0)
+      };
+
+      this.allContact.push(this.eachContact);
+      console.log(this.allContact);
+      this.showDiv = true;
+
+    }
+
+    this.first = '';
+    this.last = '';
+    this.company = '';
+    this.email = '';
+
+  }**/
 }
---
+++ src/app/models/Contact.ts
@@ -0,0 +1,12 @@
+export default class Contact {
+
+  firstName: string;
+  lastName: string;
+  company: string;
+
+  constructor(firstName: string, lastName: string, company: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.company = company;
+  }
+}
